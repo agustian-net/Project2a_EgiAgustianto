acabc520215c82cb06fbb1068c84776af3469f25
egi-agustian <egiagustianto1994@gmail.com>
2021-08-05 22:03:12 +0700

    Initial Commit

---
+++ app/src/main/java/vsga/mobile/project2a_egiagustianto/MainActivity.java
@@ -0,0 +1,14 @@
+package vsga.mobile.project2a_egiagustianto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
